469445cf73966c016500a29134fde45163a5af1e
zildevvn <vuongdevvn0509@gmail.com>
2026-07-14 15:14:36 +0700

    improve header

---
+++ tours-template.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template Name: Tours
+ */
+get_header();
+?>
+<main id="primary" class="site-main">
+    <?php get_template_part('template-parts/tours/hero-section'); ?>
+</main>
+<?php get_footer(); ?>
