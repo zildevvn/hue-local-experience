a7c252222cff943c259be9474a8bb70fb1038546
zildevvn <vuongdevvn0509@gmail.com>
2026-07-22 15:25:52 +0700

    add contact page

---
+++ contact-template.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template Name: Contact
+ */
+get_header();
+?>
+<main id="primary" class="site-main">
+    <?php get_template_part('template-parts/contact/hero-section'); ?>
+    <?php get_template_part('template-parts/contact/main-section'); ?>
+    <?php get_template_part('template-parts/shared/team-section'); ?>
+</main>
+<?php get_footer(); ?>
