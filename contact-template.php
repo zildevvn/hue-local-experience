1562d3808f9c47763c344a2aad0b46f2c69ba491
zildevvn <vuongdevvn0509@gmail.com>
2026-07-22 15:46:45 +0700

    update contact page

--- contact-template.php
+++ contact-template.php
@@ -7,6 +7,7 @@ get_header();
 <main id="primary" class="site-main">
     <?php get_template_part('template-parts/contact/hero-section'); ?>
     <?php get_template_part('template-parts/contact/main-section'); ?>
+    <?php get_template_part('template-parts/contact/map-section'); ?>
     <?php get_template_part('template-parts/shared/team-section'); ?>
 </main>
 <?php get_footer(); ?>
